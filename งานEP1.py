40a2eba610fb8a0b7649457cfb37af850a325dfd
icekub159 <125622236+icekub159@users.noreply.github.com>
2023-02-20 14:25:48 +0700

    homework ep.1

---
+++ งานEP1.py
@@ -0,0 +1,18 @@
+Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import turtle
+>>> t=turtle.Pen()
+>>> shape.('turtle')
+SyntaxError: invalid syntax
+>>> t=shape.('turtle')
+SyntaxError: invalid syntax
+>>> t.shape('turtle')
+>>> for i in range(45):
+...     t=turtle.circle(10)
+...     t=turtle.circle(20)
+...     t=turtle.circle(30)
+...     t=turtle.circle(40)
+...     t=turtle.circle(50)
+...     t=turtle.right(9)
+... 
+...     
